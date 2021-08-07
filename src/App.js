d4a4a82edb230897f200ac4c84fa026aa73c3d2d
bakkaSansanBakka <polina.potapeyko@mail.ru>
2021-08-07 20:45:22 +0300

    data in expense items changed

--- src/App.js
+++ src/App.js
@@ -6,25 +6,25 @@ import NewExpense from "./Components/NewExpense/NewExpense";
 const DUMMY_EXPENSES = [
   {
     id: "el1",
-    title: "Молоко",
+    title: "A duck",
     date: new Date(2020, 12, 28),
     amount: 1000,
   },
   {
     id: "el2",
-    title: "Оплата штрафа",
+    title: "Milk",
     date: new Date(2021, 1, 12),
     amount: 36.79,
   },
   {
     id: "el3",
-    title: "Стиральная машина",
+    title: "A Gift",
     date: new Date(2019, 10, 16),
     amount: 5300,
   },
   {
     id: "el4",
-    title: "Бытовая химия",
+    title: "Dinner in restaurant",
     date: new Date(2020, 11, 24),
     amount: 45.95,
   },
