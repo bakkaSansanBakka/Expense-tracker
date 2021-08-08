bc96083a36d93813d1ff403c50b7f4b79eda1310
bakkaSansanBakka <polina.potapeyko@mail.ru>
2021-08-08 21:42:59 +0300

    chart added

--- src/Components/Expenses/Expenses.js
+++ src/Components/Expenses/Expenses.js
@@ -3,6 +3,7 @@ import { useState } from "react";
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
 import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
 
 import "./Expenses.css";
 
@@ -23,6 +24,7 @@ const Expenses = (props) => {
           selected={filteredYear}
           onSaveFilterYear={filterHandler}
         />
+        <ExpensesChart expenses={filteredExpenses}/>
         <ExpensesList items={filteredExpenses} />
       </Card>
     </div>
